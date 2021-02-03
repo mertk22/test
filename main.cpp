1da49006950763dd776464ee569006a9f3d47e32
mertk22 <78429253+mertk22@users.noreply.github.com>
2021-02-03 18:36:05 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -13,16 +13,10 @@ int main() {
     for (int i = 0; i <integerNumber.size(); ++i) {
         integerInt =(int)(integerNumber[i] - '0');
         narcissisticValue =narcissisticValue + pow(integerInt , integerNumber.size());
-        cout << integerInt << " " ;
-        cout << integerNumber.size() << " " ;
-        cout << pow(integerInt , integerNumber.size()) << " " ;
-        cout << narcissisticValue << endl ;
     }
     stringstream convert(integerNumber);
 
     convert >> WholeIntHolder;
-    cout << WholeIntHolder << endl ;
-    cout << narcissisticValue<< endl ;
 
     if (narcissisticValue == WholeIntHolder){
         cout << "The number is a narcissistic number" ;
